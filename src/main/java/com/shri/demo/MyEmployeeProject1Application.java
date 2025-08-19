cd29975562eec33692764b883248f8ee60f4734c
Shrikantapallai <shrikantapallai363@gmail.com>
2025-08-19 23:33:38 +0530

    my First commit

--- src/main/java/com/shri/demo/MyEmployeeProject1Application.java
+++ src/main/java/com/shri/demo/MyEmployeeProject1Application.java
@@ -3,8 +3,14 @@ package com.shri.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class MyEmployeeProject1Application {
+	@PostConstruct
+	public void init() {
+		System.out.println("in it method called");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyEmployeeProject1Application.class, args);
